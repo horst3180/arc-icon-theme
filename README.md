64714801d3414295f0bb3046d2059327b87dde0c
Horst3180 <horst3180@users.noreply.github.com>
2016-06-05 15:35:46 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Arc Icon Theme
+
+**Note:** This is still unfinished. It may not work as expected in some cases.
+
+At the moment this theme mainly includes icons for folders and mimetypes.
+
+### Requirements
+
+This theme doesn't provide application icons, it needs another icon theme to inherit them.
+By default this theme will look for the Moka icon theme (http://mokaproject.com/moka-icon-theme/) to get the missing icons. If Moka is not installed it will use the Gnome icon theme as fallback.
+To change the application icons, edit `Arc/index.theme` and replace `Moka` with the name of your preferred icon theme
+
+For example, if you like the Faenza icon theme, change
+
+    [Icon Theme]
+    Name=Arc
+    Inherits=Moka,Adwaita,gnome,hicolor
+    Comment=Arc Icon theme
+
+to
+
+    [Icon Theme]
+    Name=Arc
+    Inherits=Faenza,Adwaita,gnome,hicolor
+    Comment=Arc Icon theme
+
+### Installation
+
+Installation via autotools:
+
+    git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
+    ./autogen.sh --prefix=/usr
+    sudo make install
+
+Alternatively you may copy the `Arc` folder to `~/.icons` or to `/usr/share/icons` for system-wide use.
+
+### Uninstall
+
+Run
+
+    sudo make uninstall
+
+from the same directory as this README resides in, or
+
+    sudo rm -rf /usr/share/icons/Arc
+
+
+License: GPLv3
---
+++ src/render_icons.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+INKSCAPE="/usr/bin/inkscape"
+OPTIPNG="/usr/bin/optipng"
+
+pushd `dirname $0` > /dev/null
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
+popd > /dev/null
+
+cd ${DIR}
+
+THEMEDIR=../Arc
+
+mkdir -p $THEMEDIR
+
+for CONTEXT in actions apps categories devices emblems mimetypes places status
+do
+
+    mkdir -p $THEMEDIR/$CONTEXT
+    mkdir -p $THEMEDIR/$CONTEXT
+
+    cp -r $CONTEXT/symbolic $THEMEDIR/$CONTEXT
+
+    for SIZE in 16 22 24 32 48 64 96 128
+    do
+        $INKSCAPE -S $CONTEXT.svg | grep -E "_$SIZE" | sed 's/\,.*$//' > index.tmp
+
+        mkdir -p $THEMEDIR/$CONTEXT/$SIZE
+        mkdir -p $THEMEDIR/$CONTEXT/$SIZE@2x
+
+        cp -r $CONTEXT/symlinks/* $THEMEDIR/$CONTEXT/$SIZE
+        cp -r $CONTEXT/symlinks/* $THEMEDIR/$CONTEXT/$SIZE@2x
+
+        for OBJECT_ID in `cat index.tmp`
+        do
+
+            ICON_NAME=$(sed "s/\_$SIZE.*$//" <<< $OBJECT_ID)
+
+            if [ -f $THEMEDIR/$CONTEXT/$SIZE/$ICON_NAME.png ]; then
+                echo $THEMEDIR/$CONTEXT/$SIZE/$ICON_NAME.png exists.
+            else
+                echo
+                echo Rendering $THEMEDIR/$CONTEXT/$SIZE/$ICON_NAME.png
+                $INKSCAPE --export-id=$OBJECT_ID \
+                          --export-id-only \
+                          --export-png=$THEMEDIR/$CONTEXT/$SIZE/$ICON_NAME.png $CONTEXT.svg >/dev/null \
+                && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
+            fi
+            if [ -f $THEMEDIR/$CONTEXT/$SIZE@2x/$ICON_NAME.png ]; then
+                echo $THEMEDIR/$CONTEXT/$SIZE@2x/$ICON_NAME.png exists.
+            else
+                echo
+                echo Rendering $THEMEDIR/$CONTEXT/$SIZE@2x/$ICON_NAME.png
+                $INKSCAPE --export-id=$OBJECT_ID \
+                          --export-dpi=180 \
+                          --export-id-only \
+                          --export-png=$THEMEDIR/$CONTEXT/$SIZE@2x/$ICON_NAME.png $CONTEXT.svg >/dev/null \
+                && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
+            fi
+        done
+    done
+done
+
+rm index.tmp
+cp index.theme $THEMEDIR/index.theme
+rm -rf $THEMEDIR/actions/{32,32@2x,48,48@2x,64,64@2x,96,96@2x,128,128@2x} # derp
+
+# TODO
+cp -r animations $THEMEDIR/animations
+cp -r panel $THEMEDIR/panel
